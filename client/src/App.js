9ae1a93377b12d7b14734e44ede7a5e2687bc698
letandconst <jay.delasalas0211@gmail.com>
2020-11-25 00:56:03 +0800

    create navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,38 @@
 import "./App.css";
 
+import About from "./components/NavPages/About";
+import Contact from "./components/NavPages/Contact";
+import Features from "./components/NavPages/Features";
+import Navbar from "./components/NavPages/Navbar";
+import SignUp from "./components/NavPages/SignUp";
+import Login from "./components/NavPages/Login";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar></Navbar>
+
+        <Switch>
+          <Route path="/about-us">
+            <About />
+          </Route>
+          <Route path="/features">
+            <Features />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/sign-up">
+            <SignUp />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/components/NavPages/SignUp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div>register</div>;
+};
+
+export default SignUp;
