66d2949233755407e824bfe41122c710935fe158
FordanG <60175691+FordanG@users.noreply.github.com>
2020-11-27 07:00:13 +0800

    Update Login.js

--- client/src/components/NavPages/Login.js
+++ client/src/components/NavPages/Login.js
@@ -7,30 +7,31 @@ const Login = () => {
 
   return (
     <>
-      <div className="bg-grey-lighter flex flex-col my-32">
+      <div className="flex flex-col my-32">
         <div className="container bg-white max-w-2xl mx-auto flex-1 flex flex-col items-center justify-center px-2">
-          <div className=" px-20 py-8 rounded-3xl shadow-lg text-black w-full my-15 text-center">
+          <div className="px-20 py-8 rounded-3xl subtle-shadow text-black w-full my-15 text-center">
             <form onSubmit={handleSubmit(onSubmit)}>
-              <h1 className="mb-8 text-3xl text-center">Sign In</h1>
+              <h1 className="mb-8 text-2xl font-bold uppercase text-center">
+                Sign In
+              </h1>
               <input
                 type="email"
                 name="email"
-                defaultValue="Your Email"
+                placeholder="Your Email"
                 ref={login}
-                className="block border rounded-xl border-grey-light w-full p-3 mb-4"
               />
 
               <input
                 type="password"
                 name="password"
-                defaultValue="Password"
+                placeholder="Password"
                 ref={login}
-                className="block border rounded-xl border-grey-light w-full p-3 mb-4"
+                className=""
               />
 
               <button
                 type="submit"
-                className="btn primary-btn w-full py-3 my-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg rounded-full focus:outline-none hover:bg-gray-900 hover:shadow-none"
+                className="btn primary-btn subtle-shadow w-full py-3 my-6 font-medium tracking-widest text-white uppercase focus:outline-none hover:bg-gray-900 hover:shadow-none"
               >
                 Login
               </button>
