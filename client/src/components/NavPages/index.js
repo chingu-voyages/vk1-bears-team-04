813d9dfeb5a332355a1cb7fde496e0b9c7f595dc
FordanG <60175691+FordanG@users.noreply.github.com>
2020-11-27 07:51:05 +0800

    Create index.js

---
+++ client/src/components/NavPages/index.js
@@ -0,0 +1,9 @@
+import { FormProvider } from "react-hook-form";
+
+export { default as About } from "./About";
+export { default as Contact } from "./Contact";
+export { default as Features } from "./Features";
+export { default as Footer } from "./Footer";
+export { default as Login } from "./Login";
+export { default as Navbar } from "./Navbar";
+export { default as SignUp } from "./SignUp";
