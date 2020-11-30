4e1e72dd347a3af4f549a4f006d70b52bbf92a83
FordanG <60175691+FordanG@users.noreply.github.com>
2020-11-30 21:33:23 +0800

    Update PrivateRoute.js

--- client/src/components/Auth/PrivateRoute.js
+++ client/src/components/Auth/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from "react-router-dom";
 
 const PrivateRoute = (props) => {
   // Add Validation Here
-  const condition = false;
+  const condition = true;
 
   return condition ? (
     <Route path={props.path} exact={props.exact} component={props.component} />
