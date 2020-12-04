93c2dc792940bb2819d9df02e286e603727d065d
Diane-Besmonte <elsiebesmonte@gmail.com>
2020-12-04 23:08:09 +0800

    forgot password and page routes

---
+++ client/src/components/NavPages/Privacy.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Privacy = () => {
+  return (
+    <div>
+      <h1>Welcome to <span className="text-red text-md hover:text-blue-500">Privacy Policy</span> page</h1>
+    </div>
+  );
+};
+
+export default Privacy;
